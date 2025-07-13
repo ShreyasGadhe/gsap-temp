57e6fee4e59c3789439697d56870f245ba4cdb4a
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-07-14 00:57:13 +0530

    added scrolltrigger video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 import { SplitText } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -41,6 +44,24 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: videoRef.current,
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
   return (
     <>
@@ -59,14 +80,18 @@ const Hero = () => {
 
         <div className="body">
           <div className="content">
-            <div className="space-y-5 hidden md:block">
-              <p>Cool. Crisp. Classic.</p>
-              <p className="subtitle">
-                Sip the Spirit <br /> of Summer
-              </p>
+            <div className="">
+              <div className="gradient-border space-y-5 hidden p-6 md:block shadow-lg bg-linear-60 from-white/30 to-white/0">
+                <p text-white text-sm>
+                  Cool. Crisp. Classic.
+                </p>
+                <p className="subtitle ">
+                  Sip the Spirit <br /> of Summer
+                </p>
+              </div>
             </div>
             <div className="view-cocktails w-[50vw] h-50 lg:w-full">
-              <p className="subtitle">
+              <p className="subtitle hidden md:block">
                 Every cocktail on our menu is a blend of premium ingredients,
                 creative flair, and timeless recipes - designed to delight your
                 senses.
@@ -76,6 +101,15 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-8">
+        <video
+          ref={videoRef}
+          src="/videos/input.mp4"
+          muted
+          playsInline
+          preload="auto"
+        ></video>
+      </div>
     </>
   );
 };
