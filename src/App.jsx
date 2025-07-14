40e4463e299eae417612251fd7e2478da425bc4c
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-07-15 01:38:07 +0530

    deployment error fix(again)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 
 import Hero from "./components/Hero";
-import Cocktails from "./components/cocktails";
+import Cocktails from "./components/cocktails.jsx";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
